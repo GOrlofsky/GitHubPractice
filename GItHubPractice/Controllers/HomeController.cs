7a9f681146bf01f8076dbdb0a12f02725721a58e
Gitty Orlofsky <gittyorlo@gmail.com>
2024-03-26 12:17:51 -0400

    Add project files.

---
+++ GItHubPractice/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using GitHubPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GitHubPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GItHubPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitHubPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
